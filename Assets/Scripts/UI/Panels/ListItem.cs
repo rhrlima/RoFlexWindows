d456eca771ca749b09e131744a2c993e2b95e32d
rhrlima <ricardo.hrlima@gmail.com>
2025-11-13 23:47:07 -0300

    feat: Cleaner version of List Modal

--- Assets/Scripts/UI/Panels/ListItem.cs
+++ Assets/Scripts/UI/Panels/ListItem.cs
@@ -8,16 +8,20 @@ using UnityEngine.UI;
 public class ListItem : MonoBehaviour, ISelectHandler, IPointerClickHandler, ISubmitHandler
 {
     private ListPanel panel;
-    private Image image;
-    [HideInInspector] public int index;
-
+    private int _index;
     [SerializeField] private TMP_Text _optionText;
-
     [SerializeField] private ListItemEvent _onSelectEvent;
     [SerializeField] private ListItemEvent _onSubmitEvent;
     [SerializeField] private ListItemEvent _onClickEvent;
 
-    public string OptionText {
+    public int Index
+    {
+        get => _index;
+        set => _index = value;
+    }
+
+    public string OptionText
+    {
         get => _optionText.text;
         set => _optionText.text = value;
     }
@@ -45,51 +49,25 @@ public class ListItem : MonoBehaviour, ISelectHandler, IPointerClickHandler, ISu
         panel = GetComponentInParent<ListPanel>();
         if (panel == null)
             Debug.LogError("ListPanel script not found in parent.");
-
-        image = GetComponent<Image>();
-        if (image == null)
-            Debug.LogError("Image component not found in list item.");
-    }
-
-    public void OnOptionEnter()
-    {
-        SetAlpha(1.0f);
-    }
-
-    public void OnOptionExit()
-    {
-        SetAlpha(0f);
-    }
-
-    private void SetAlpha(float alphaValue)
-    {
-        Color color = image.color;
-        color.a = alphaValue;
-        image.color = color;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        // panel.SelectOption(index);
+        Debug.Log($"CLICKED {_index}");
         _onClickEvent.Invoke(this);
-
-        Debug.Log($"CLICKED {index}");
     }
 
     public void OnSelect(BaseEventData eventData)
     {
+        Debug.Log($"SELECTED {_index}");
         _onSelectEvent.Invoke(this);
-
-        Debug.Log($"SELECTED {index}");
-
-        panel.autoScroll.FitOptiontoView(index);
+        // panel.autoScroll.FitOptiontoView(index);
     }
 
     public void OnSubmit(BaseEventData eventData)
     {
+        Debug.Log($"SUBMITED {_index}");
         _onSubmitEvent.Invoke(this);
-
-        Debug.Log($"SUBMITED {index}");
     }
 
     public void ObtainSelectionFocus()
--- Assets/Scripts/UI/Panels/ListPanel.cs
+++ Assets/Scripts/UI/Panels/ListPanel.cs
@@ -1,36 +1,29 @@
-/*
-arrow navigation vs scroll bar
-auto scroll when navigating the list (disable navigation in scroll component)
-*/
-
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ListPanel : MonoBehaviour
 {
+    public AutoScroll autoScroll;
+    public int numOptions;
     [SerializeField] private List<ListItem> listOptions;
     [SerializeField] private ListItem listItemPrefab;
-    public int numOptions;
-    private int selectedIndex = -1;
-
     [SerializeField] private ListItemEvent _onOptionSelectEvent;
     [SerializeField] private ListItemEvent _onOptionSubmitEvent;
     [SerializeField] private ListItemEvent _onOptionClickEvent;
-
-    public AutoScroll autoScroll;
 
     public ListItemEvent OnOptionSelectEvent
     {
         get => _onOptionSelectEvent;
         set => _onOptionSelectEvent = value;
     }
+
     public ListItemEvent OnOptionSubmitEvent
     {
         get => _onOptionSubmitEvent;
         set => _onOptionSubmitEvent = value;
     }
+
     public ListItemEvent OnOptionClickEvent
     {
         get => _onOptionClickEvent;
@@ -70,7 +63,7 @@ public class ListPanel : MonoBehaviour
         for (int i = 0; i < numOptions; i++)
         {
             ListItem newItem = Instantiate(listItemPrefab, transform);
-            newItem.index = i;
+            newItem.Index = i;
             newItem.OptionText = $"Option {i + 1}";
 
             newItem.OnSelectEvent.AddListener((ListItem) => HandleOnOptionSelect(newItem));
@@ -82,21 +75,7 @@ public class ListPanel : MonoBehaviour
     private void HandleOnOptionSelect(ListItem option)
     {
         _onOptionSelectEvent.Invoke(option);
-        // FitOptiontoView(option.index);
-    }
-
-    public void RefreshOptions(int newIndex)
-    {
-        if (newIndex < 0 || newIndex >= listOptions.Count) return;
-
-        if (newIndex == selectedIndex) return;
-
-        if (selectedIndex >= 0 && selectedIndex < listOptions.Count)
-            listOptions[selectedIndex].OnOptionExit();
-
-        listOptions[newIndex].OnOptionEnter();
-
-        selectedIndex = newIndex;
+        autoScroll.FitOptiontoView(option.Index);
     }
 
     public void SelectOption(int index)
@@ -104,12 +83,5 @@ public class ListPanel : MonoBehaviour
         GameObject option = transform.GetChild(index).gameObject;
         var item = option.GetComponent<ListItem>();
         item.ObtainSelectionFocus();
-
-        // FitOptiontoView(index);
-    }
-
-    public int GetSelectedOption()
-    {
-        return selectedIndex;
     }
 }
