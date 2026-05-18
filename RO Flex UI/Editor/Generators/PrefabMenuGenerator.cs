4fc61e22585d5123eeb7cc9ffa05cdfe61b14cca
rhrlima <ricardo.hrlima@gmail.com>
2026-05-18 19:07:38 -0300

    refac: Ignore generated script

--- RO Flex UI/Editor/Generators/PrefabMenuGenerator.cs
+++ RO Flex UI/Editor/Generators/PrefabMenuGenerator.cs
@@ -30,6 +30,7 @@ namespace RoFlexUI.Editor
         }}
 ";
 
+        [InitializeOnLoadMethod]
         [MenuItem("Tools/RO Flex UI/Regenerate Prefab Menu")]
         public static void BakeMenu()
         {
