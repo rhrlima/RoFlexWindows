da3781957134e9d5c5a5a3257511145caa6d7b83
rhrlima <ricardo.hrlima@gmail.com>
2026-05-25 14:11:43 -0300

    fix: Material in font asset generator

--- RO Flex UI/Editor/Generators/FontAssetGenerator.cs
+++ RO Flex UI/Editor/Generators/FontAssetGenerator.cs
@@ -52,48 +52,36 @@ namespace RO_Flex_UI.Editor
             fontAsset.atlasTexture.hideFlags = HideFlags.None;
 
             fontAsset.TryAddCharacters(characters, true);
+            fontAsset.atlasPopulationMode = AtlasPopulationMode.Static;
 
             fontAsset.atlasTexture.name = sourceFont.name + " Atlas";
 
             fontAsset.atlasTexture.filterMode = FilterMode.Point;
             fontAsset.atlasTexture.wrapMode = TextureWrapMode.Clamp;
 
-            // var shader = Shader.Find("TextMeshPro/Distance Field");
             var shader = Shader.Find("TextMeshPro/Bitmap");
-            var material = new Material(shader);
+            var material = new Material(shader)
+            {
+                name = sourceFont.name + " Material"
+            };
 
-            material.name = sourceFont.name + " Material";
-
-            material.SetTexture("_MainTex", fontAsset.atlasTexture);
+            material.SetTexture(ShaderUtilities.ID_MainTex, fontAsset.atlasTexture);
 
             fontAsset.material = material;
+            fontAsset.atlasTextures = new[] { fontAsset.atlasTexture };
 
             AssetDatabase.CreateAsset(fontAsset, assetPath);
             AssetDatabase.AddObjectToAsset(fontAsset.atlasTexture, fontAsset);
             AssetDatabase.AddObjectToAsset(fontAsset.material, fontAsset);
 
-            EditorUtility.SetDirty(fontAsset.atlasTexture);
-            EditorUtility.SetDirty(fontAsset.material);
             EditorUtility.SetDirty(fontAsset);
+            EditorUtility.SetDirty(fontAsset.atlasTexture);
+            EditorUtility.SetDirty(material);
 
             AssetDatabase.SaveAssets();
-            AssetDatabase.ImportAsset(assetPath, ImportAssetOptions.ForceUpdate);
-
-            ConvertAtlasToStatic(fontAsset);
+            AssetDatabase.Refresh();
 
             Debug.Log($"Successfully generated asset at {assetPath}");
-        }
-
-        private static void ConvertAtlasToStatic(TMP_FontAsset fontAsset)
-        {
-            if (fontAsset == null) return;
-
-            fontAsset.atlasPopulationMode = AtlasPopulationMode.Static;
-
-            EditorUtility.SetDirty(fontAsset);
-            AssetDatabase.SaveAssets();
-
-            Debug.Log("Converted atlas to static for font asset: " + fontAsset.name);
         }
     }
 }
---
+++ TODO.md
@@ -0,0 +1,24 @@
+- TODO Simple panels are currently an empty object with a sprite.
+  - Create Panel component to define background via script for skin change.
+
+- TODO Draggable:
+ - Lock in bounds
+ - Return to original area (you can move, but it snaps back)
+ - OnDrag Event
+
+- FIXME spacing in buttons to panel for tabs vertical
+
+- TODO Components could have fields and vars to facilitate configure child objects (i.e text)
+
+- TODO create repeating item panel
+
+- TODO text panel with auto scroll
+
+- TODO global config
+ - Font
+ - Skin
+ - Language
+
+- FIXME Resize and anchor position (window growing from the center)
+
+- TODO add ROSlider to scroll panel
